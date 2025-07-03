41d2fe5861161204fb9509c39ddef85386c92234
sadaffatima-10 <sadaffatima189@gmail.com>
2025-07-04 02:39:48 +0530

    Time: 1 ms (33.76%), Space: 41.6 MB (43.94%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -1,23 +1,36 @@
+import java.util.Stack;
+
 public class Solution {
     public boolean checkValidString(String s) {
-        int low = 0, high = 0;
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> starStack = new Stack<>();
 
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                low++;
-                high++;
-            } else if (c == ')') {
-                low--;
-                high--;
-            } else { 
-                low--;      
-                high++;     
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                leftStack.push(i);
+            } else if (ch == '*') {
+                starStack.push(i);
+            } else { // ')'
+                if (!leftStack.isEmpty()) {
+                    leftStack.pop();
+                } else if (!starStack.isEmpty()) {
+                    starStack.pop();
+                } else {
+                    return false;
+                }
             }
-
-            if (high < 0) return false;
-            if (low < 0) low = 0;
         }
 
-        return low == 0;
+        while (!leftStack.isEmpty() && !starStack.isEmpty()) {
+            if (leftStack.peek() > starStack.peek()) {
+                return false;
+            }
+            leftStack.pop();
+            starStack.pop();
+        }
+
+        return leftStack.isEmpty(); 
     }
 }
