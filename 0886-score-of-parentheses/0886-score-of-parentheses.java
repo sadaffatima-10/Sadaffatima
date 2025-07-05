ba7dd0ceead05505568243bd391d8b8bd4efc39f
sadaffatima-10 <sadaffatima189@gmail.com>
2025-07-05 21:36:02 +0530

    Time: 1 ms (46.98%), Space: 41.7 MB (27.05%) - LeetHub

--- 0886-score-of-parentheses/0886-score-of-parentheses.java
+++ 0886-score-of-parentheses/0886-score-of-parentheses.java
@@ -2,21 +2,26 @@ class Solution {
     public int scoreOfParentheses(String s) {
         Stack<Integer> stack = new Stack<>();
 
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
+        for(char c : s.toCharArray()){
+            if(c == '('){
                 stack.push(0);
-            } else {
+            }
+            else{
                 int val = 0;
-                while (stack.peek() != 0) {
+                while(stack.peek() != 0){
                     val += stack.pop();
                 }
                 stack.pop();
-                stack.push(val == 0 ? 1 : 2 * val);
+                if(val == 0){
+                    stack.push(1);
+                }
+                else{
+                    stack.push(2 * val);
+                }
             }
         }
-
         int score = 0;
-        while (!stack.isEmpty()) {
+        while(!stack.isEmpty()){
             score += stack.pop();
         }
 
