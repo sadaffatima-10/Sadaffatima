26ac7515ed44b9cd04c3120e86afee3ef4b37695
sadaffatima-10 <sadaffatima189@gmail.com>
2025-07-08 02:42:56 +0530

    Time: 4 ms (33.97%), Space: 44.5 MB (11.76%) - LeetHub

--- 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
+++ 1570-final-prices-with-a-special-discount-in-a-shop/1570-final-prices-with-a-special-discount-in-a-shop.java
@@ -5,15 +5,12 @@ class Solution {
         Stack<Integer> stack = new Stack<>();
 
         for (int i = n - 1; i >= 0; i--) {
-            // Remove all elements greater than current price
             while (!stack.isEmpty() && stack.peek() > prices[i]) {
                 stack.pop();
             }
 
-            // If stack is not empty, top is the discount
             result[i] = prices[i] - (stack.isEmpty() ? 0 : stack.peek());
 
-            // Push current price for future elements
             stack.push(prices[i]);
         }
 
