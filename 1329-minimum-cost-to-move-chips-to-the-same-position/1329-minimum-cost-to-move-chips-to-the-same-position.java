8e5c12f5402e2d95c2b01ba0cd93616af7e43649
sadaffatima-10 <sadaffatima189@gmail.com>
2025-08-10 00:12:20 +0530

    Time: 0 ms (100%), Space: 41.6 MB (12.64%) - LeetHub

---
+++ 1329-minimum-cost-to-move-chips-to-the-same-position/1329-minimum-cost-to-move-chips-to-the-same-position.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int minCostToMoveChips(int[] position) {
+        int evenCount = 0;
+        int oddCount = 0;
+        
+        for (int pos : position) {
+            if (pos % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
+            }
+        }
+        
+        return Math.min(evenCount, oddCount);
+    }
+}
