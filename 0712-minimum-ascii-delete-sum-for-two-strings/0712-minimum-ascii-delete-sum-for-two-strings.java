427f0d2fb2783266d0b2702d15daa36ed8eb4abc
Sadaf Fatima <sadaffatima189@gmail.com>
2025-09-17 21:38:22 +0530

    Time: 18 ms (82.16%), Space: 44.8 MB (78.6%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,43 +1,27 @@
 class Solution {
     public int minimumDeleteSum(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
+        int m = s1.length(), n = s2.length();
         int[][] dp = new int[m+1][n+1];
-
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-
-
-        return solve(s1, s2, 0, 0, dp);
-    }
-
-    private int solve(String s1, String s2,int i, int j, int[][] dp){
-        int m = s1.length();
-        int n = s2.length();
-
-        if(i >= m && j >= n){
-            return 0;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        if( i >= m){
-            return dp[i][j] = s2.charAt(j) + solve(s1,s2, i, j+1, dp);
-        }
-        if( j >= n){
-            return dp[i][j] = s1.charAt(i) + solve(s1,s2, i+1, j, dp);
-        }
-
-        if(s1.charAt(i) == s2.charAt(j)){
-            return dp[i][j] = solve(s1, s2, i+1, j+1, dp);
-        }
-       
-        int delete_s1 = s1.charAt(i) + solve(s1, s2, i+1, j, dp);
-        int delete_s2 = s2.charAt(j) + solve(s1, s2, i, j+1, dp);
         
-        return dp[i][j] = Math.min(delete_s1, delete_s2);
-
+        for (int i = m-1; i >= 0; i--) {
+            dp[i][n] = dp[i+1][n] + s1.charAt(i);
+        }
+        for (int j = n-1; j >= 0; j--) {
+            dp[m][j] = dp[m][j+1] + s2.charAt(j);
+        }
+        
+        for (int i = m-1; i >= 0; i--) {
+            for (int j = n-1; j >= 0; j--) {
+                if (s1.charAt(i) == s2.charAt(j)) {
+                    dp[i][j] = dp[i+1][j+1];
+                } else {
+                    int deleteS1 = s1.charAt(i) + dp[i+1][j];
+                    int deleteS2 = s2.charAt(j) + dp[i][j+1];
+                    dp[i][j] = Math.min(deleteS1, deleteS2);
+                }
+            }
+        }
+        
+        return dp[0][0];
     }
 }
