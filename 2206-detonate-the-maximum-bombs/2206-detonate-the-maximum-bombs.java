d38552b5e7d548ed59741a8ac2a664e597fcc8a4
Sadaf Fatima <sadaffatima189@gmail.com>
2025-10-11 00:16:13 +0530

    Time: 81 ms (78.64%), Space: 45.1 MB (45.87%) - LeetHub

---
+++ 2206-detonate-the-maximum-bombs/2206-detonate-the-maximum-bombs.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int maximumDetonation(int[][] bombs) {
+        int n = bombs.length;
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+
+        for (int i = 0; i < n; i++) {
+            long x1 = bombs[i][0], y1 = bombs[i][1], r1 = bombs[i][2];
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                long x2 = bombs[j][0], y2 = bombs[j][1];
+                long dx = x1 - x2, dy = y1 - y2;
+                if (dx * dx + dy * dy <= r1 * r1) {
+                    graph.get(i).add(j);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            boolean[] visited = new boolean[n];
+            int count = dfs(graph, visited, i);
+            max = Math.max(max, count);
+        }
+
+        return max;
+    }
+
+    private int dfs(List<List<Integer>> graph, boolean[] visited, int node) {
+        visited[node] = true;
+        int count = 1;
+        for (int nei : graph.get(node)) {
+            if (!visited[nei]) {
+                count += dfs(graph, visited, nei);
+            }
+        }
+        return count;
+    }
+}
