0a98242f2bbcf551ef16b299a15e0c2174035de9
Sadaf Fatima <sadaffatima189@gmail.com>
2025-10-30 01:08:11 +0530

    Time: 0 ms (100%), Space: 57.9 MB (8.4%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -3,14 +3,18 @@ class Solution {
         int n = nums.length;
         k = k % n;
 
-        int[] temp = new int[n];
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, n - 1);
+    }
 
-        for (int i = 0; i < n; i++) {
-            temp[(i + k) % n] = nums[i];
-        }
-
-        for (int i = 0; i < n; i++) {
-            nums[i] = temp[i];
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
     }
 }
