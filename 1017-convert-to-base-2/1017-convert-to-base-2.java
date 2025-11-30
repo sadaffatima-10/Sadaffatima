1e901ed4293532298f3bb473d9bb53a23b399c61
Sadaf Fatima <sadaffatima189@gmail.com>
2025-11-30 19:15:57 +0530

    Time: 1 ms (72.68%), Space: 42.8 MB (17.53%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String baseNeg2(int n) {
+        if(n == 0){
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while(n!=0){
+            int rem = n % -2;
+            n = n/ -2;
+
+            if(rem < 0){
+                rem += 2;
+                n = n+1;
+            }
+
+            sb.append(rem);
+        }
+
+        return sb.reverse().toString();
+    }
+}
