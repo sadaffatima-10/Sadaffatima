71af7e2caf4705e58a52c877dda2659cb5536da3
Sadaf Fatima <sadaffatima189@gmail.com>
2025-12-01 11:57:42 +0530

    Time: 1 ms (99.88%), Space: 94.4 MB (19.8%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -3,14 +3,13 @@ class Solution {
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
 
-        for (int price : prices) {
-            if (price < minPrice) {
+        for(int price : prices){
+            if( price < minPrice){
                 minPrice = price;
-            } else if (price - minPrice > maxProfit) {
+            }else if( price - minPrice > maxProfit){
                 maxProfit = price - minPrice;
             }
         }
-
         return maxProfit;
     }
 }
