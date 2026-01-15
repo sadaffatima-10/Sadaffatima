b60ce8acb1244b3d1a124c09d20654702d77abe5
Sadaf Fatima <sadaffatima189@gmail.com>
2026-01-16 00:00:55 +0530

    Time: 4 ms (99.78%), Space: 45.7 MB (93.85%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0 || ( x%10 ==0 && x != 0 )){
+            return false;
+        }
+        int reversedHalf = 0;
+        while(x > reversedHalf){
+            int digit = x % 10;
+            reversedHalf = reversedHalf * 10 + digit;
+            x = x/10;
+        }
+        return (reversedHalf == x) || (x == reversedHalf/10); 
+    }
+}
